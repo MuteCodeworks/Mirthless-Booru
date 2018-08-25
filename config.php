52b71584849088a518a7059974fa12905ad2d466
MirthlessMachine <42687888+MirthlessMachine@users.noreply.github.com>
2018-08-25 18:40:22 -0400

    Update config.php
    "blanked" config file and added descriptions

--- config.php
+++ config.php
@@ -1,13 +1,22 @@
 <?php
-$title = "M.M";
-$imagedir = "filecachedatabase/";
+// title displayed at the top of the page
+$title = "";
+
+// content storage file
+$imagedir = "";
+
+// thumbnail file (this shouldnt change)
 $thumbdir = "thumbs/";
-//absolute path to booru
+
+// absolute path to booru installation
 $imgck = "";
+
 //
 $stylesheet = "style.css";
-$mysql_host = "localhost";
-$mysql_database = "localbooru";
-$mysql_user = "root";
+
+// sql host, database name, user, and password
+$mysql_host = "";
+$mysql_database = "";
+$mysql_user = "";
 $mysql_password = "";
 ?>
