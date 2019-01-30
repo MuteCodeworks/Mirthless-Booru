6375460216a5f259e49992ba1efcc42fd78f3eea
MirthlessMachine <42687888+MirthlessMachine@users.noreply.github.com>
2019-01-29 20:13:03 -0500

    Update edit-pool.php

--- Booru-Files/edit-pool.php
+++ Booru-Files/edit-pool.php
@@ -82,7 +82,7 @@
 								$postq = mysqli_query($link , $query) or die(mysqli_error($link));
 								if (!mysqli_num_rows($postq)==0){
 									$row = mysqli_fetch_array($postq);
-									display_post($link,$metaterms, $postq, $row);
+									display_post($link,$metaterms, $postq, $row, $thumbdir);
 								}
 								$pool_map_result = mysqli_fetch_array($query_pool_map);
 							}
