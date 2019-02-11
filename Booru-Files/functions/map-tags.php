cfd79e83696dd95dd1c0a5da999aa81c085b7b97
MirthlessMachine <42687888+MirthlessMachine@users.noreply.github.com>
2019-02-11 16:43:25 -0500

    Add files via upload

--- Booru-Files/functions/map-tags.php
+++ Booru-Files/functions/map-tags.php
@@ -79,87 +79,76 @@
 			$tag_array = array_unique($tag_array);
 			$run = count($tag_array);
 			$update_tagmap = "DELETE FROM tagmap WHERE post_id = $post_id";
-
-
+			mysqli_query($link,$update_tagmap)or die(mysqli_error($link));
+			
 			for( $i = 0 ; $i < $run ; $i++ ){
-				$query = "SELECT id , tag_id FROM tags t , tagmap tm WHERE t.tagfull='$tag_array[$i]' AND tm.tag_id=t.id";
-				$tag_result = mysqli_query($link,$query) or die(mysqli_error($link));
-				$tag_row = mysqli_fetch_array($tag_result);
-				if(mysqli_num_rows($tag_result)==0){
-					$query = "SELECT id FROM tags WHERE tagfull='$tag_array[$i]'LIMIT 1";
-					$result_exists = mysqli_query($link,$query) or die(mysqli_error($link));
-					if(mysqli_num_rows($result_exists)==0){
+				$query = "SELECT tagalias , id FROM tags WHERE tagfull='$tag_array[$i]'LIMIT 1";
+				$result_exists = mysqli_query($link,$query) or die(mysqli_error($link));
+				if(mysqli_num_rows($result_exists)!=0){
+					//
+					echo "run<br />";
+					$id_row = mysqli_fetch_array($result_exists);
+					//
+					echo "$id_row[id]:$id_row[tagalias]<br />";
+					$query = "INSERT INTO tagmap VALUES ( \"$post_id:EDIT:$i\" , $post_id , $id_row[id] )";
+					//
+					echo "$query<br />";
+					$result = mysqli_query($link , $query) or die(mysqli_error($link));
+					print_r($result);
+				}
+				else{
+					for( $o = 0 ; $o < $meta ; $o++ ){
+						
+						$check = strstr($tag_array[$i],"($metaterms[$o])");
+						if($check){
 
-						$has_meta = false;
-						$meta = count($metaterms);
-						for( $o = 0 ; $o < $meta ; $o++ ){
-
-							$check = strstr($tag_array[$i],"($metaterms[$o])");
-							if($check){
-
-								$type = substr($check, strrpos($check,'(')+1 , strrpos($check,')')-strrpos($check,'(')-1);
-								if($type!=$metaterms[$o]){
-
-								}
-								else{
-									$tag = substr($tag_array[$i], 0, strrpos($tag_array[$i],"_($metaterms[$o])"));
-									$queryexists = "SELECT * FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
-									$sqlexists = mysqli_query($link, $queryexists)or die(mysqli_error($link));
-									if(mysqli_num_rows($sqlexists)==0){
-
-										$queryupdate = "INSERT INTO tags VALUES ( id ,'$type' , '$tag_array[$i]' , '$tag' )";
-										$update = mysqli_query($link, $queryupdate)or die(mysqli_error($link));
-										$query = "SELECT MAX(id) AS id FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
-										$result = mysqli_query($link,$query);
-										$row = mysqli_fetch_array($result);
-
-									}
-									else{
-										$query = "SELECT id FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
-										$result = mysqli_query($link,$query);
-										$row = mysqli_fetch_array($result);
-									}
-									$has_meta = true;
-								}
-							}
-						}
-						if(!$has_meta){
-							$type = "general";
-							$queryexists = "SELECT * FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
-							$sqlexists = mysqli_query($link, $queryexists)or die(mysqli_error($link));
-							if(mysqli_num_rows($sqlexists)==0){
-
-								$queryupdate = "INSERT INTO tags VALUES ( id ,'$type' , '$tag_array[$i]' , '$tag_array[$i]' )";
-								$update = mysqli_query($link, $queryupdate)or die(mysqli_error($link));
-								$query = "SELECT MAX(id) AS id FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
-								$result = mysqli_query($link,$query);
-								$row = mysqli_fetch_array($result);
+							$type = substr($check, strrpos($check,'(')+1 , strrpos($check,')')-strrpos($check,'(')-1);
+							if($type!=$metaterms[$o]){
 
 							}
 							else{
-								$query = "SELECT id FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
-								$result = mysqli_query($link,$query);
-								$row = mysqli_fetch_array($result);
+								$tag = substr($tag_array[$i], 0, strrpos($tag_array[$i],"_($metaterms[$o])"));
+								$queryexists = "SELECT * FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
+								$sqlexists = mysqli_query($link, $queryexists)or die(mysqli_error($link));
+								if(mysqli_num_rows($sqlexists)==0){
+
+									$queryupdate = "INSERT INTO tags VALUES ( id ,'$type' , '$tag_array[$i]' , '$tag' )";
+									$update = mysqli_query($link, $queryupdate)or die(mysqli_error($link));
+									$query = "SELECT MAX(id) AS id FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
+									$result = mysqli_query($link,$query);
+									$row = mysqli_fetch_array($result);
+
+								}
+								else{
+									$query = "SELECT id FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
+									$result = mysqli_query($link,$query);
+									$row = mysqli_fetch_array($result);
+								}
+								$has_meta = true;
 							}
 						}
-						$query = "INSERT INTO tagmap VALUES ( \"$post_id:EDIT:$i\" , $post_id , $row[id] )";
-						$result = mysqli_query($link , $query) or die(mysqli_error($link));
-						$update_tagmap .=" AND tag_id != $row[id]";
 					}
-					else{
-						$id_row = mysqli_fetch_array($result_exists);
-						$query = "INSERT INTO tagmap VALUES ( \"$post_id:EDIT:$i\" , $post_id , $id_row[id] )";
+					if(!$has_meta){
+						$type = "general";
+						$queryexists = "SELECT * FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
+						$sqlexists = mysqli_query($link, $queryexists)or die(mysqli_error($link));
+						if(mysqli_num_rows($sqlexists)==0){
 
-						$result = mysqli_query($link , $query) or die(mysqli_error($link));
-						$update_tagmap .=" AND tag_id != $id_row[id]";
+							$queryupdate = "INSERT INTO tags VALUES ( id ,'$type' , '$tag_array[$i]' , '$tag_array[$i]' )";
+							$update = mysqli_query($link, $queryupdate)or die(mysqli_error($link));
+							$query = "SELECT MAX(id) AS id FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
+							$result = mysqli_query($link,$query);
+							$row = mysqli_fetch_array($result);
+
+						}
+						else{
+							$query = "SELECT id FROM tags WHERE tagfull='$tag_array[$i]' AND type='$type' LIMIT 1";
+							$result = mysqli_query($link,$query);
+							$row = mysqli_fetch_array($result);
+						}
 					}
-				}
-				else{
-
-					$update_tagmap .=" AND tag_id != $tag_row[id]";
 				}
 			}
-			mysqli_query($link,$update_tagmap) or die(mysqli_error($link));
 		}
 		elseif($run_as=='REMOVE'){
 			$query = "DELETE FROM tagmap WHERE post_id = $post_id";
